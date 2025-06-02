f1ec564727e1103582d6ff2353fb3b98d13d7bde
JBC <joci1358@student.su.se>
2025-06-03 01:02:07 +0200

    I decide to conduct an ARIMA anaylisi on the price of future, I now started to conduct a stationary analyisis

--- 3_Pre_Var_Analysis.R
+++ 3_Pre_Var_Analysis.R
@@ -1,4 +1,122 @@
 
+
+
+# Load the logged data
+coffe_data <- read_csv("Raw_Data/Coffee_Data_Set.csv")
+coffe_future_series <- coffe_data$Close_USD_60kg 
+ADF_coffe_futures <- ur.df(coffe_future_series, type = "none", lags = 0)
+ADF_coffe_futures_lagged <- ur.df(coffe_future_series, type = "none", selectlags = "AIC", lags = 12)
+ADF_coffe_futures_lagged_drift<- ur.df(coffe_future_series, type = "drift", selectlags = "AIC", lags = 12) # + intercept
+ADF_coffe_futures_lagged_trend <- ur.df(coffe_future_series, type = "trend", selectlags = "AIC", lags = 40) # + intercept & trend
+
+diff_coffee <- diff(coffe_future_series)
+ADF_coffe_futures_diff_coffee <- ur.df(diff_coffee, type = "drift", selectlags = "AIC", lags = 0)
+
+summary(ADF_coffe_futures) # 0.6464 > -1.95, fail to reject the null hypothesis (Not Stationary).
+summary(ADF_coffe_futures_lagged) # 0.5122> -1.95, fail to reject the null hypothesis (Not Stationary).
+summary(ADF_coffe_futures_lagged_drift)# -1.1654 > -1.95, fail to reject the null hypothesis (Not Stationary).
+summary(ADF_coffe_futures_lagged_trend) # -1.2927 > -1.95, fail to reject the null hypothesis (Not Stationary).
+summary(ADF_coffe_futures_diff_coffee) # -77.7866 < -1.95, reject the null hypothesis(Stationary).
+summary(ur.kpss(coffe_future_series)) # 15.9817 > 0.739, Reject null hypothesis(Not Stationary).
+
+
+
+############################################################################################
+
+# We now want to "identify, estimate and do diagnostics of at least one ARIMA-model". We know 
+# from the tests above that we want to estimate an ARIMA(p,1,q) for some p and q, i.e. an 
+# ARMA(p,q) model for the difference of the real M1 money stock.
+
+# Plotting the estimated autocorrelation function
+
+Acf(dm)
+
+# An ARMA(1,1) or MA(1) seems like good candidates (why?). Let's first use the arima() function
+# to estimate and check coefficients.
+
+MA1 <- arima(dm, order = c(0,0,1), method = "ML")
+ARMA11 <- arima(dm, order = c(1,0,1), method = "ML")
+
+summary(MA1)
+summary(ARMA11)
+
+############################################################################################
+
+# Ljung-Box test
+
+# Let's start by plotting the residuals and check the ACF of both models
+
+par(mfrow=c(2,2)) # to plot the four plots below in a 2 x 2 grid
+plot(MA1$residuals,
+     main = "MA(1) Residual",
+     ylab = "Residual",
+     xlab = "",
+)
+abline(h = mean(MA1$residuals),
+       lty = "dashed",
+       lwd = 2,
+       col = "blue")
+
+plot(ARMA11$residuals,
+     main = "ARMA(1,1) Residual",
+     ylab = "Residual",
+     xlab = "",
+)
+abline(h = mean(ARMA11$residuals),
+       lty = "dashed",
+       lwd = 2,
+       col = "blue")
+
+Acf(MA1$residuals, 
+    main = "MA(1) Residual",
+    ylab = "ACF",
+    xlab = "")
+
+Acf(ARMA11$residuals, 
+    main = "ARMA(1,1) Residual",
+    ylab = "ACF",
+    xlab = "")
+
+# It is not unexpected that these plots look similar for both series (why?) but both look 
+# good!
+
+# We now perform Ljung-Box tests. First we compute the appropriate number of lags to choose 
+# using the the usual rule of thumb.
+
+q <- floor(0.75*nobs(MA1)^(1/3)) # = 4
+
+# Then compute the number of parameters for our two models.
+
+ncoeff_MA1 <- length(coef(MA1)) # = 2
+ncoeff_ARMA11 <- length(coef(ARMA11)) # = 3
+
+# Then use the LjungBox function to perform the LjungBox test. Remember that the null 
+# hypothesis of the Ljung-Box test is autocorrelation = 0 for lags 1,...,q.
+
+LjungBox(MA1$residuals, lags = q, fitdf = ncoeff_MA1)
+LjungBox(ARMA11$residuals, lags = q, fitdf = ncoeff_ARMA11)
+
+# Can not reject the null in both cases which does not surprise me given the plot.
+
+# Finally we want to rank our two models using AIC and BIC (Bayesian information
+# criterion). Recall that:
+
+# AIC: 2k - 2log(L),
+# BIC: log(n)k - 2log(L),
+
+# and remember that lower is better!
+
+IC <- data.frame(
+  Model = c("MA(1)", "ARMA(1,1)"),
+  AIC = c(AIC(MA1), AIC(ARMA11)),
+  BIC = c(BIC(MA1), BIC(ARMA11))
+)
+
+IC
+
+
+
+
 ################################################Classic Dickey-Fuller test###########################################
 
 # Load required packages
@@ -47,7 +165,6 @@ summary(DF_log_return_arabica) # -76.43 < -1.95,  strongly reject the null hypot
 summary(DF_log_return_robusta) #−75.44 < -1.95, strongly reject the null hypothesis.
 summary(DF_log_return_exhange_rate)# -70.5777 < -1.95, strongly reject the null hypothesis.
 summary(DF_log_return_coffe_futures) # -70.5777 < -1.95, strongly reject the null hypothesis.
-
 
 
 #################################Kwiatkowski–Phillips–Schmidt–Shin (KPSS) #########################################
@@ -146,6 +263,18 @@ summary(ur.kpss(dm))
 
 # Note that the larger the test statistic, the lower the p-value; i.e. we "want" a small test 
 # statistic to not reject the null hypothesis of trend stationarity.
+
+
+
+
+
+
+
+
+
+
+
+
 
 ############################################################################################
 
