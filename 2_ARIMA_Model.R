bdb9bbc1fd4b1d95ceb51bfbf9e2141ea78bb8ec
JBC <joci1358@student.su.se>
2025-06-06 14:34:26 +0200

    Perforermed white nose, arima, and arma test, determing the white noise is best.

--- 2_ARIMA_Model.R
+++ 2_ARIMA_Model.R
@@ -22,38 +22,113 @@ arabica_spot_xts <- xts(coffee_data$Price_Arabica, order.by = coffee_data$Date)
 arabica_futures_xts <- xts(coffee_data$Close_USD_60kg, order.by = coffee_data$Date)
 
 # 3. Merge series on common dates
-data_xts <- merge(merge(arabica_spot_xts, robusta_spot_xts, join = "inner"))
+data_xts <- merge(merge(arabica_spot_xts, arabica_futures_xts, join = "inner"))
 colnames(data_xts) <- c("arabica_spot_price", "arabica_futures_price")
 
 # 4. Difference of Logs
-diff_log_spot_price_arabica <- na.omit(diff(log_spot_price_arabica))
-diff_log_futures_price_arabica <- na.omit(diff(log_futures_price_arabica))
+diff_log_spot_price_arabica <- na.omit(diff(log(arabica_spot_xts)))
+diff_log_futures_price_arabica <- na.omit(diff(log(arabica_futures_xts)))
 
 
+##########################################################################################################
+###                               1. ARIMA ARABICA SPOT PRICE                                          ### 
 
+
+#ACF PLOT
 png("Processed_Data/acf_plot_spot_price.png", width = 800, height = 600)
 acf(diff_log_spot_price_arabica, main = "ACF Plot Spot Price Arabica")
 legend("topright", legend = c("Significant Lag", "Non-significant Lag"),
        fill = c("blue", "grey"), border = NA, bty = "n")
 dev.off()
+#ACF don’t show significant structure I will conduct ARIMA  
 
+arima_spot_price_log_diff_white_noise <- arima(diff_log_spot_price_arabica, order = c(0,0,0), method = "ML")
+arima_spot_price_log_diff_MA <- arima(diff_log_spot_price_arabica, order = c(0,0,1), method = "ML")
+arima_spot_price_log_diff_ARMA <- arima(diff_log_spot_price_arabica, order = c(1,0,1), method = "ML")
+
+summary(arima_spot_price_log_diff_white_noise)
+#Only an intercept term: 0.0004
+#AIC: -28409.84
+#RMSE: 0.0196, MAE: 0.0146
+#Residuals ACF1: -0.017
+
+summary(arima_spot_price_log_diff_MA)
+#MA1: -0.0185 (not statistically significant, s.e. = 0.0139)
+#AIC: -28409.61 (slightly worse than (0,0,0))
+#RMSE: 0.0196, MAE: 0.0146
+#Residuals ACF1: 0.0007
+
+summary(arima_spot_price_log_diff_ARMA)
+#AR1: 0.3265, MA1: -0.3517 (both with large standard errors)
+#AIC: -28409.83 (slightly better than (0,0,1), slightly worse than (0,0,0))
+#RMSE: 0.0196, MAE: 0.0146
+#Residuals ACF1: 0.0074
+
+#Conclusion: Although the AIC is slightly improved over MA(1), the standard errors are large, meaning 
+#parameter estimates are not precise, a white noise seems better.
+
+
+
+
+##########################################################################################################
+###                               1. ARIMA ARABICA FUTURE PRICE                                          ### 
+
+#ACF PLOT
 png("Processed_Data/acf_plot_future_price.png", width = 800, height = 600)
 acf(diff_log_spot_price_arabica, main = "ACF Plot Futures Price Arabica")
 legend("topright", legend = c("Significant Lag", "Non-significant Lag"),
        fill = c("blue", "grey"), border = NA, bty = "n")
 dev.off()
+# Indicates sm
 
-#ACF don’t show significant structure I will conduct ARIMA model using
+arima_future_price_log_diff_white_noise  <- arima(diff_log_futures_price_arabica, order = c(0,0,0), method = "ML")
+arima_future_price_log_diff_MA  <- arima(diff_log_futures_price_arabica, order = c(0,0,1), method = "ML")
+arima_future_price_log_diff_ARMA <- arima(diff_log_futures_price_arabica, order = c(1,0,1), method = "ML")
+
+summary(arima_future_price_log_diff_white_noise)
+#Intercept: 0.0004
+#AIC: -27440.27 -> lowest AIC among all
+#RMSE: 0.02138
+#MAE: 0.01596
+#Residual ACF1: -0.0036
+
+summary(arima_future_price_log_diff_MA)
+#MA1: -0.0037 (very close to zero, and not significant, s.e. = 0.0136)
+#AIC: -27438.35 (worse than white noise)
+#Error metrics: Nearly identical to ARIMA(0,0,0)
+#Residual ACF1: ~0
+
+summary(arima_future_price_log_diff_ARMA)
+# AR1 & MA1: Both near zero and not estimated properly (s.e. = NaN → parameters not identifiable)
+#AIC: -27436.34 (worst of all three models)
+#RMSE/MAE: Identical to others
+#Residual ACF1: ~0
+
+#Adding a MA(1) term does not improve the model, and might be unnecessary complexity. NaNs in standard 
+#errors suggest model overfitting / collinearity / numerical issues in the ARMA model.
+#Conclusion: Once again the white noise model fits well and has the lowest AIC, meaning it's the most 
+#parsimonious with decent predictive accuracy.
+
+##########################################################################################################
+###                               Ljung-box                                                   ### 
 
 
-arima_spot_price_log_diff <- arima(diff_log_spot_price_arabica, order = c(0,0,0), method = "ML")
-arima_future_price_log_diff  <- arima(diff_log_futures_price_arabica, order = c(0,0,0), method = "ML")
 
-summary(arima_spot)
-summary(arima_futures)
+
+
+
+
+
 
 #Since the ARIMA(0,0,0) basically assumes white noise, the Ljung-Box test will confirm if that assumption holds—i.e., residuals are uncorrelated.
 # If the test shows no significant autocorrelation, you’re good. If it does, you may want to consider more complex AR or MA terms.
+
+
+
+
+
+
+
 
 
 ##############3
