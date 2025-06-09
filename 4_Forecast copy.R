ac5d8e37a1e515767d77db6c418c7c15e7a022af
JBC <joci1358@student.su.se>
2025-06-09 17:21:48 +0200

    Changes

--- 4_Forecast copy.R
+++ 4_Forecast copy.R
@@ -3,20 +3,40 @@
 ##########################################################################################################
 
 
-# Read and prepare data
-coffee_data <- read_csv("Raw_Data/Coffee_Data_Set.csv", show_col_types = FALSE) %>%
-  mutate(Date = as.Date(Date, format = "%Y-%m-%d")) %>%
-  arrange(Date)
+# Package installation
+# Load or install required packages
+required_packages <- c("readr", "dplyr", "xts", "urca", "dynlm", "vars", "forecast")
 
-# Create xts objects
-arabica_xts <- xts(coffee_data$Price_Arabica, order.by = coffee_data$Date)
-robusta_xts <- xts(coffee_data$Price_Robusta, order.by = coffee_data$Date)
+installed <- required_packages %in% installed.packages()
+if (any(!installed)) {
+  install.packages(required_packages[!installed])
+}
+invisible(lapply(required_packages, library, character.only = TRUE))
+
+# Data preparation #
+# 1. Read CSV and preprocess
+coffee_data <- read_csv("Raw_Data/Coffee_Data_Set.csv")
+
+# 2. Create xts objects
+arabica_spot_xts <- xts(coffee_data$Price_Arabica, order.by = coffee_data$Date)
+robusta_spot_xts <- xts(coffee_data$Price_Robusta, order.by = coffee_data$Date)
 arabica_futures_xts <- xts(coffee_data$Close_USD_60kg, order.by = coffee_data$Date)
+usd_real_exchange_xts <- xts(coffee_data$PTAX, order.by = coffee_data$Date)
 
-# Merge and clean
-merged_prices <- merge(arabica_xts, robusta_xts, arabica_futures_xts)
-colnames(merged_prices) <- c("Arabica", "Robusta", "Arabica Futures")
-merged_prices <- merged_prices[complete.cases(merged_prices), ]
+# 3. Merge series on common dates
+# Merge the four xts objects with inner join on common dates
+data_xts <- merge(arabica_spot_xts, robusta_spot_xts, arabica_futures_xts, usd_real_exchange_xts, join = "inner")
+
+# Rename columns
+colnames(data_xts) <- c("arabica_spot_price", "robusta_spot_price", "arabica_futures_price", "usd_real_exchange")
+
+
+diff_log_spot_price_arabica <- na.omit(diff(log(arabica_spot_xts)))
+diff_log_spot_price_robusta <- na.omit(diff(log(robusta_spot_xts)))
+diff_log_futures_price_arabica <- na.omit(diff(log(arabica_futures_xts)))
+diff_log_usd_real_exchange <- na.omit(diff(log(usd_real_exchange_xts)))
+
+
 
 # Save PNG
 png("Processed_Data/graph_1_coffee_prices_with_futures.png", width = 1200, height = 600)
